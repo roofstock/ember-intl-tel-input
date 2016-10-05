3f4de3b19e595343b335116aa5fb6769c2a1d575
Matt Armstrong <matt@roofstock.com>
2016-10-05 02:12:40 -0700

    fixing binding of property change

--- addon/components/intl-tel-input.js
+++ addon/components/intl-tel-input.js
@@ -314,13 +314,7 @@ export default Ember.TextField.extend({
    * @method setupIntlTelInput
    */
   setupIntlTelInput: Ember.on('didInsertElement', function() {
-    var notifyPropertyChange = Ember.observer('value', () => {
-      var number = this.get('value');
-      if (number) {
-        that.$().intlTelInput('setNumber', number);
-        that.set('deferNumber', number);
-      }
-    });
+    var notifyPropertyChange = this.notifyPropertyChange.bind(this, 'value');
     var that = this;
     Ember.run.scheduleOnce('afterRender', function() {
 
