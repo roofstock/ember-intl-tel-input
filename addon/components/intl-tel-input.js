aa27296b3b037d4454d80290eb59bda6af4e895c
Matt Armstrong <matt@roofstock.com>
2017-08-24 11:51:58 -0700

    Fixed issue with control crashing when jquery reference is undefined.

--- addon/components/intl-tel-input.js
+++ addon/components/intl-tel-input.js
@@ -222,7 +222,7 @@ export default Ember.TextField.extend({
     },
     set: function(key, number) {
       if (typeof number === 'undefined' || number === '') { return; }
-      if (this.element === null || this.$() === null) {
+      if (this.element === null || !this.$()) {
         // schedule to set after element is created
         this.set('deferNumber', number);
       } else {
